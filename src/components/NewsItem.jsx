9eb3b79851ea2a0e42235e2c83e3bfc2a9bf3886
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-03-25 13:27:48 +0100

    changes

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { showNewsItem } from "../api/Api";
+import "../App.css";
 
-function NewsItem({ newsId }) {
+function NewsItem({ newsId, no }) {
   const [newsItem, setNewsItem] = useState({});
 
-  console.log(newsId);
+  //   console.log(newsId);
 
   useEffect(() => {
     showNewsItem(newsId).then((data) => {
@@ -13,17 +14,27 @@ function NewsItem({ newsId }) {
     });
   }, [newsId]);
 
-  console.log(newsItem);
+  const setTime = (d) => {
+    let newTime = Date.now() - d * 1000;
+    let hrs = Math.floor(newTime / (1000 * 60 * 60));
+    return hrs;
+  };
 
-  const { title, by, id, url } = newsItem;
+  const shortenUrl = (url) => {
+    url && (url = url.substring(8, 23) + "...");
+    return url;
+  };
+
+  const { title, by, id, url, time } = newsItem;
 
   return (
-    <>
+    <div className="news" key={id}>
+      {no}
       <h1>{title}</h1>
-      <p>{by}</p>
-      <p>{id}</p>
-      <a href={url}>Full story</a>
-    </>
+      <p>By {by}</p>
+      <p>{setTime(time)} hrs ago</p>
+      <a href={url}>Full story ({shortenUrl(url)})</a>
+    </div>
   );
 }
 
